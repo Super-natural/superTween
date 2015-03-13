bbdc7f0685f7e71c1c984cf015766b88141a7f58
Steve U <generalstephen1@gmail.com>
2015-03-13 15:57:20 +0000

    Including Dev
    Dev/_js/superTween.js is an unmagnified version of the code

---
+++ Dev/_js/superTween_prefix.js
@@ -0,0 +1,18 @@
+/*
+ * Super natural's micro Tween Engine
+ * http://www.wearesupernatural.com/
+ * Based on Robert Penners Easing functions
+ * v0.1.1, last commit 02/03/15
+ * currently supports down to IE8
+ */
+
+
+
+/* @param elem: element to be tweened
+ * @param time: length of animation
+ * @param obj{
+ *	 		delay: Dime to delay (in seconds)
+ *			onComplete: Function to run after animation completion
+ *			onCompleteParams: parameters for the oncomplete function
+ *			ease: ease to use for tween
+*/
